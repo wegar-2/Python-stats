03a6bbba324e0d61eeea20e72ca4b8b95832c5da
wegar-2 <prometheus0209@gmail.com>
2017-10-22 15:58:52 +0200

    Update of basic commands memo: granting and revoking of privileges has been added.

--- MySQL_basics/how_to_basic_admin_commands.sql
+++ MySQL_basics/how_to_basic_admin_commands.sql
@@ -18,3 +18,17 @@ CREATE USER 'the_user'@'localhost' identified by 'user_account_password';
 ### 6. Deleting a user
 DROP USER IF EXISTS 'the_user'@'localhost';
 
+### 7. Granting privileges to a user:
+GRANT ALL PRIVILEGES ON DATABASE_NAME.* TO 'the_user'@'localhost';
+
+### 8. What are the different privileges that can be endowed?
+### 8.1. CREATE
+### 8.2. DELETE
+### 8.3. INSERT
+### 8.4. SELECT
+### 8.5. UPDATE
+
+### 9. REVOKING the prvileges that have been granted
+
+REVOKE SELECT, INSERT ON DATABASE_NAME.TABLE_NAME FROM 'the_user'@'localhost';
+
