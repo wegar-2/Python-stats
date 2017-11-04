d2f8cb085ba20643882dce559fb858110667b7d9
wegar-2 <prometheus0209@gmail.com>
2017-11-04 16:43:03 +0100

    Adding work on lesson about generators.

--- 1_python_foundations/7_generators_in_Python.py
+++ 1_python_foundations/7_generators_in_Python.py
@@ -1,3 +1,79 @@
 ### This script is a walk through the article at:
 # https://jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
 
+import math
+import pickle
+
+
+# 0. define exceptions that will be used later on
+class NotInteger(Exception):
+    pass
+
+
+class NegativeArgument(Exception):
+    pass
+
+
+class NotList(Exception):
+    pass
+
+
+# 1. The first problem that is posed is recognizing prime numbers in a list of numbers.
+# To this end, an algorithm is needed to say whether a given number is a prime number.
+def eratosthenes_sieve(n):
+    """
+    This function checks whether its input is a prime number
+    :param n: a nonnegative integer
+    :return: boolean saying whther n is prime
+    """
+    # check the input
+    if not isinstance(n, int):
+        raise NotInteger("Function eratosthenes_sieve got non-integer argument!")
+    elif n < 0:
+        raise NegativeArgument("Function eratosthenes_sieve got negative integer argument!")
+    if n == 0 or n == 1:
+        return False
+    else:
+        # walk through divisors up to sqrt of the input and check the residual
+        for divisor in range(2, int(math.floor(math.sqrt(n)))+1, 1):
+            if n % divisor == 0:
+                return True
+        # if none of those numbers turned out to be a divisor, return False
+        return False
+
+
+def select_primes_from_list(list_in):
+    """
+    This function takes in a list and filters out its elements that are not integers
+    :param list_in: list to be filtered
+    :return: list of integers that are prime numbers
+    """
+    # check the input
+    if not isinstance(list_in, list):
+        raise NotList("The argument passed to select_primes_from_list is not a list!")
+    # iterate through the elements of the list
+    primes_list = list()
+    for item in list_in:
+        try:
+            # it might happen that list element is not an integer...
+            if eratosthenes_sieve(item):
+                primes_list.append(item)
+        except NotInteger:
+            print("List element is not an integer!")
+        except NegativeArgument:
+            print("List element is negative!")
+        except Exception:
+            print("General error in select_primes_from_list!")
+    return primes_list
+
+test_list = list(range(0, 20))
+print(test_list)
+print(select_primes_from_list(test_list))
+
+# 2. Now, for the purpose of illustration, create a very big list of consecutive integers and show that if the size
+# gets bigger it becomes problematic to work with such objects
+list_of_integers = list()
+for iter in range(100000000):
+    list_of_integers.append(iter)
+
+
