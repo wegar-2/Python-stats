89aaa25f71773c8e374bd1ae22a1aa83343bc526
wegar-2 <prometheus0209@gmail.com>
2017-11-06 22:36:50 +0100

    Added one more idiome regarding dicitonaries.

--- 1_python_foundations/selected_python_idioms.py
+++ 1_python_foundations/selected_python_idioms.py
@@ -1,5 +1,6 @@
 import string
 import numpy as np
+import itertools
 
 # 1. how to elegantly iterate over a list with iteration number
 my_list = list(string.ascii_lowercase[0:10])
@@ -30,3 +31,37 @@ print(leonidas_quote)
 # 3.2. Now, the elegant way
 leonidas_quote = " ".join(list_of_strings)
 print(leonidas_quote)
+
+# 4. cartesian product in Python: library itertools needed
+# 4.1. cartesian product of sets
+set1 = {"a", "b", "q", "w", "u", "d"}
+set2 = {2, 5, 4, 78}
+set1_x_set2 = list(itertools.product(set1, set2)) # note that order of the input structures is not retained
+# 4.2. cartesian product of two lists
+list1 = [1, 2, 3, 4, 5]
+list2 = ["a", "c", "v"]
+list1_x_list2 = list(itertools.product(list1, list2)) # order is retained
+# Sidenote: itertools.product() accepts iterables as its argument
+# Iterable - class that has method __iter__ defined
+# Iterator - class that has method __next__ defined
+
+# 5. Initializing a dictionary's entries
+# 5.1. first example
+group1 = ["a", "b", "c"]
+group2 = [3, 6, 10]
+group3 = [22, 2]
+list_of_tuples = list(itertools.product(group1, group2, group3))
+
+my_dict_2 = {} # empty dictionary; sidenote: set1 = set() to produce an empty dictionary
+for (el1, el2, el3) in list_of_tuples:
+    # iterating over a list of tuple
+    my_dict_2[el1] = my_dict_2.get(el1, 0) + el2*el3
+# 5.2. second example
+group1 = ["a", "b"]
+group2 = [4, 5]
+group3 = [1, 2]
+list_of_tuples = list(itertools.product(group1, group2, group3))
+test_dict = {}
+for (el1, el2, el3) in list_of_tuples:
+    test_dict[el1] = test_dict.get(el1, 0) + el2*el3
+    
