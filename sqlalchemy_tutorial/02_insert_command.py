8952b7b8746e0c5a557ba618d642f37a591ce9c9
wegar-2 <prometheus0209@gmail.com>
2018-05-01 16:17:32 +0200

    Completed simple script on sqlalchemy's INSERT() command.

--- sqlalchemy_tutorial/02_insert_command.py
+++ sqlalchemy_tutorial/02_insert_command.py
@@ -6,6 +6,11 @@ import string
 engine1 = sa.create_engine("mysql://test_user:test1234@localhost/test_db")
 connection1 = engine1.connect()
 
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# 1. using raw SQL insert command
+
 # raw SQL insert statement
 insert_raw_cmd = "INSERT INTO table_1 (varchar_column, int_column) VALUES ('asdf', 123);"
 connection1.execute(insert_raw_cmd)
@@ -23,4 +28,25 @@ for el in range(10000):
                iter_word + "' , " + str(iter_num[0]) + ");"
     connection1.execute(iter_cmd)
 
+connection1.close()
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# 2. using SQLAlchemy's insert() command
+engine2 = sa.create_engine("mysql+mysqldb://test_user:test1234@localhost/test_db")
+connection2 = engine2.connect()
+metadata2 = sa.MetaData(bind=engine2)
+
+my_table = sa.Table("my_table", metadata2,
+                    sa.Column("index_column", sa.INT, nullable=False, autoincrement=True),
+                    sa.Column("text_column", sa.VARCHAR(20), nullable=False)
+                    )
+metadata2.create_all()
+
+ins_cmd = my_table.insert().values(index_column=1, text_column="qwerty")
+connection2.execute(ins_cmd)
+connection2.close()
+# ----------------------------------------------------------------------------------------------------------------------
 
